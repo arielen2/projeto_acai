c1378496824c8adb6724a191a4a08759c8cc2118
arielen vitor <139460288+arielen2@users.noreply.github.com>
2023-07-25 11:58:22 -0300

    Add files via upload

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sorvete_pedido, name='pedido')
+    path('', views.sorvete_pedido, name='pedido'),
+    path('lista', views.pedidos, name='lista_pedidos'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import SorveteForm
+from .models import Sorvete
+
 def sorvete_pedido(request):
     if request.method == 'POST':
         form = SorveteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('pedido') #redireciona para a pasgina de sucesso apos salvar
+            return redirect('pedido') #redireciona para a pagina de sucesso apos salvar
     else:
         form = SorveteForm()
 
     return render(request,'main/index.html', {'form': form})
+
+def pedidos(request):
+    query = Sorvete.objects.all()
+    return render(request,'main/lista.html', {'query':query})
